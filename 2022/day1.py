1cc6d7713bf9ee48534088b0a240cca3e19de408
alangron <74920718+alangron@users.noreply.github.com>
2022-12-01 15:37:12 +0000

    Day 1

---
+++ 2022/day1.py
@@ -0,0 +1,37 @@
+
+import pandas as pd
+
+# opening the file in read mode
+my_file = open("C:/Users/Administrator/Documents/GitHub/AdventOfCode/2022/Data/day1.txt", "r")
+  
+# reading the file in to a list
+data = my_file.read()
+df = data.split("\n")
+
+for i in range(len(df)):
+    if df[i] == '':
+        df[i] = '-1'
+
+
+df = [int(x) for x in df]
+
+# Part 1
+total = 0
+maxcal = 0
+vals = []
+
+for x in range(0,len(df)):
+    if df[x] != -1:
+        total = total + df[x] 
+    else:
+        if total > maxcal:
+            maxcal = total
+        vals = vals + [total]
+        total = 0
+        
+print(maxcal)
+
+
+# Part 2
+vals.sort(reverse=True)
+print(sum(vals[0:3]))
