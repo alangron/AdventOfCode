296e1e044eb78829009979df02b33cedbe967851
alangron <74920718+alangron@users.noreply.github.com>
2022-12-02 16:21:55 +0000

    Update day2

--- 2022/day2.py
+++ 2022/day2.py
@@ -7,51 +7,20 @@ df = pd.read_csv(filename, sep='\s+')
 
 # Part 1
 
-# Convert data to numeric and subtract them
+# Convert data to numeric
 df = df.replace({'A','X'},'1').replace({'B','Y'},'2').replace({'C','Z'},'3')
 
-df['result'] = (df['col2'].astype(int) - df['col1'].astype(int)) % 3
+# Take the difference between 2 values and add one. This gives 0 for a loss, 1 for a draw and 2 for a win
+df['result'] = (df['col2'].astype(int) - df['col1'].astype(int) + 1) % 3
 
-# A difference of 1 is a win, 0 is a draw and 2 is a loss
-for x in range(0,len(df)):
-    if df.loc[x,'result'] == 0:
-        df.loc[x,'score'] = 3
-    if df.loc[x,'result'] == 1:
-        df.loc[x,'score'] = 6
-    if df.loc[x,'result'] == 2:
-        df.loc[x,'score'] = 0
-        
-print(sum(df['col2'].astype(int))+sum(df['score']))
-        
+# Sum the initial secltion with 3x the result for part 1 answer
+print(sum(df['col2'].astype(int))+3*sum(df['result']))
+
 
 # Part 2
-df = pd.read_csv(filename, sep='\s+')
-
-df = df.replace({'A'},'1').replace({'B'},'2').replace({'C'},'3')
-df = df.replace({'X'},'-1').replace({'Y'},'0').replace({'Z'},'1')
-
-df['pick'] = (df['col2'].astype(int) + df['col1'].astype(int)) % 3
-
-for x in range(0,len(df)):
-    if df.loc[x,'pick'] == 0:
-        df.loc[x,'pick'] = 3
+df['pick'] = (df['col2'].astype(int) + df['col1'].astype(int) - 2) % 3
+df['pick'] = df['pick'].replace(0,3)
          
-for x in range(0,len(df)):
-    if df.loc[x,'col2'] == '-1':
-        df.loc[x,'score'] = 0
-    if df.loc[x,'col2'] == '0':
-        df.loc[x,'score'] = 3
-    if df.loc[x,'col2'] == '1':
-        df.loc[x,'score'] = 6
-         
-         
-print(sum(df['pick'].astype(int))+sum(df['score']))
-         
-
-       
-        
-        
-        
-        
-        
+print(sum(3*(df['col2'].astype(int)-1))+sum(df['pick']))
+             
         
