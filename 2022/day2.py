9fe7db015409479643c4889ca1f925cbbca1cf82
alangron <74920718+alangron@users.noreply.github.com>
2022-12-03 13:32:29 +0000

    day3

--- 2022/day2.py
+++ 2022/day2.py
@@ -2,25 +2,19 @@ import pandas as pd
 
 # opening the file in read mode
 filename = "Data/day2.csv"
-
 df = pd.read_csv(filename, sep='\s+')
-
-# Part 1
-
 # Convert data to numeric
 df = df.replace({'A','X'},'1').replace({'B','Y'},'2').replace({'C','Z'},'3')
 
+# Part 1
 # Take the difference between 2 values and add one. This gives 0 for a loss, 1 for a draw and 2 for a win
 df['result'] = (df['col2'].astype(int) - df['col1'].astype(int) + 1) % 3
 
 # Sum the initial secltion with 3x the result for part 1 answer
 print(sum(df['col2'].astype(int))+3*sum(df['result']))
 
-
 # Part 2
 df['pick'] = (df['col2'].astype(int) + df['col1'].astype(int) - 2) % 3
 df['pick'] = df['pick'].replace(0,3)
          
 print(sum(3*(df['col2'].astype(int)-1))+sum(df['pick']))
-             
-        
